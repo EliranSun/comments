ae02d6f445a3bca20ebdaeae1b9deb31dfeb63ca
RyanMcklain <piro27@gmail.com>
2022-01-16 14:33:57 +0200

    adjust threshold

--- src/components/EndlessScroll/EndlessScroll.js
+++ src/components/EndlessScroll/EndlessScroll.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
 const ITEMS_PER_PAGE = 20;
-const SCROLL_THRESHOLD = 100;
-const INIT_THRESHOLD = 500;
 
 const EndlessScroll = ({ children }) => {
   const [itemsLimit, setItemsLimit] = useState(ITEMS_PER_PAGE);
-  const [threshold, setThreshold] = useState(INIT_THRESHOLD);
+  const [threshold, setThreshold] = useState(0);
 
   return children({
     itemsLimit,
     loadMoreItems: (event) => {
-      if (event.target.scrollTop > threshold) {
+      const { scrollHeight, scrollTop } = event.target;
+      const initThreshold = (scrollHeight * 2) / 3;
+      if (scrollTop > (threshold || initThreshold)) {
         setItemsLimit(itemsLimit + ITEMS_PER_PAGE);
-        setThreshold(event.target.scrollHeight - SCROLL_THRESHOLD);
+        setThreshold(scrollHeight);
       }
     },
   });
